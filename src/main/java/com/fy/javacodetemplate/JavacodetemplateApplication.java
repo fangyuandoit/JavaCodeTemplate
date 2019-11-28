7f2f7440dc4948edab78b7eca46157e0f1d2a976
fangyuandoit <676176407@qq.com>
2019-11-28 22:43:54 +0800

    create project

---
+++ src/main/java/com/fy/javacodetemplate/JavacodetemplateApplication.java
@@ -0,0 +1,13 @@
+package com.fy.javacodetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodetemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacodetemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fy/javacodetemplate/JavacodetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fy.javacodetemplate;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacodetemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
